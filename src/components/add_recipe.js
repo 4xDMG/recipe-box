4b3352ffe35a10cd8c5ed8d66e4fd9827704064f
4xDMG <dajtait87@gmail.com>
2017-01-20 16:57:24 +1000

    Started working on AddRecipe component

--- src/components/add_recipe.js
+++ src/components/add_recipe.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const AddRecipe = () => {
-	return <button>Add Recipe</button>
+class AddRecipe extends Component {
+	displayRecipeDialog(id) {
+		if (document.getElementById(id).classList.contains("hidden")) {
+			document.getElementById(id).classList.remove("hidden");
+		} else {
+			document.getElementById(id).classList.add("hidden");
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={() => this.displayRecipeDialog('add-recipe-dialog')}>Add Recipe</button>
+				<div id="add-recipe-dialog" className="dialog hidden">
+        			<form>
+         		 		<label htmlFor="recipe-name">Recipe Name: </label>
+          				<input type="text" name="recipe-name" />
+          				<br />
+          				<label htmlFor="ingredients">Ingredients (Seperated by a comma): </label>
+          				<input type="text" name="ingredients" />
+          				<br />
+          				<input type="submit" value="Submit" />
+        			</form>
+      			</div>
+      		</div>
+		)
+	}
 }
 
 export default AddRecipe;
